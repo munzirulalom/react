72737e5d98c48701876d118663ab7b461e2bf60a
munzirulalom <cse.alomb008@gmail.com>
2024-08-19 23:15:37 +0600

    Understing the react flow

---
+++ 01_basic/src/App.jsx
@@ -0,0 +1,14 @@
+import Youtube from "./Youtube"
+
+function App() {
+  return (
+    <>
+      <h1>Hello World 1</h1>
+      <h1>Hello World 2</h1>
+
+      <Youtube/>
+    </>
+  )
+}
+
+export default App
---
+++ 01_basic/src/Youtube.jsx
@@ -0,0 +1,10 @@
+function Youtube () {
+    return (
+        <>
+            <h1>Hello Youtube</h1>
+            <h1>Hello Youtube 2</h1>
+        </>
+    )
+}
+
+export default Youtube
