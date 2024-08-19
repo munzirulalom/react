29d91bc2514ca6c4ad846bae1b4b043d130e1588
munzirulalom <cse.alomb008@gmail.com>
2024-08-20 00:01:56 +0600

    Create own react library & JSX

--- 01_basic/src/main.jsx
+++ 01_basic/src/main.jsx
@@ -1,7 +1,23 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+const reactElement = {
+    type: 'a',
+    props : {
+        href: "https://google.com",
+        target: "_blank"
+    },
+    children: "Clcil me to visit google"
+}
+
+const CustomReactElement = React.createElement(
+    reactElement.type,
+    reactElement.props,
+    reactElement.children
+)
+
 createRoot(document.getElementById('root')).render(
-    <App />
+    // <App />
+    CustomReactElement
 )
