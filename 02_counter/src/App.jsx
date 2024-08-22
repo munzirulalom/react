70abde51205ce521e579f28da7ac25e51bbe8615
munzirulalom <cse.alomb008@gmail.com>
2024-08-22 08:00:25 +0600

    Learn `useState` hooks

---
+++ 02_counter/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [counter, setCounter] = useState(15);
+  // let counter = 15;
+
+  const addValue = () => {
+    /** Due to the React batching it always give or increment 1 when call the method */
+    // setCounter(counter + 1);
+    // setCounter(counter + 1);
+    // setCounter(counter + 1);
+    // setCounter(counter + 1);
+
+    /** To avoid the issue we can used an callback method into the setCounter function */
+    setCounter((prevCounter) => prevCounter + 1); // preValue is previous value from the react state
+    setCounter((prevCounter) => prevCounter + 1); // preValue is previous value from the react state
+    setCounter((prevCounter) => prevCounter + 1); // preValue is previous value from the react state
+    setCounter((prevCounter) => prevCounter + 1); // preValue is previous value from the react state
+  };
+
+  const removeValue = () => {
+    setCounter(counter - 1);
+  };
+
+  return (
+    <>
+      <h1>React course from hitesh {counter}</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button onClick={addValue}>Add value</button>
+      <button onClick={removeValue}>Remove value</button>
+
+      <p>Footer: {counter}</p>
+    </>
+  );
+}
+
+export default App;
