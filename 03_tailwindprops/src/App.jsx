3912724716eb28d38b31ab9ce9c114f9d0eed71a
munzirulalom <cse.alomb008@gmail.com>
2024-08-26 23:30:34 +0600

    Passing `obj` data to the `<Card />` component

--- 03_tailwindprops/src/App.jsx
+++ 03_tailwindprops/src/App.jsx
@@ -19,13 +19,17 @@ function App() {
       title: 'Card 4',
       img: 'https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80',
     },
+    {
+      title: 'Card 5',
+      img: 'https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80',
+    },
   ]
 
   return (
     <>
       <div className='grid grid-cols-3 gap-5'>
         {datas.map((item) => (
-          <Card title={item.title} img={item.img} />
+          <Card data={item} />
         ))}
       </div>
     </>
--- 03_tailwindprops/src/components/Card.jsx
+++ 03_tailwindprops/src/components/Card.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
 
-export default function Card({
-  title = 'No Value',
-  img = 'https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80',
-}) {
+export default function Card({ data }) {
   return (
     <>
       <div className='relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96'>
         <div className='relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40'>
-          <img src={img} alt='card-image' />
+          <img src={data.img} alt='card-image' />
         </div>
         <div className='p-6'>
           <h5 className='block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900'>
-            {title}
+            {data.title}
           </h5>
           <p className='block font-sans text-base antialiased font-light leading-relaxed text-inherit'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet atque
